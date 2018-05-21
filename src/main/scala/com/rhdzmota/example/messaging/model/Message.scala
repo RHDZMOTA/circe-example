7deec03e49e7b034b839525183d1b2b7e8d78605
rhdzmota <rohdzmota@gmail.com>
2018-05-21 12:29:33 -0500

    irrelevant changes

--- src/main/scala/com/rhdzmota/example/messaging/model/Message.scala
+++ src/main/scala/com/rhdzmota/example/messaging/model/Message.scala
@@ -2,8 +2,7 @@ package com.rhdzmota.example.messaging.model
 
 sealed trait Message {
   def timestamp: Int
-  def content: Option[String]
 }
 
-case class Text(timestamp: Int, content: Option[String]) extends Message
-case class WithAttachment(timestamp: Int, content: Option[String], attachment: Option[Attachment]) extends Message
+case class Text(timestamp: Int, content: String) extends Message
+case class WithAttachment(timestamp: Int, content: Option[String], attachment: Attachment) extends Message
